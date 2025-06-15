a9b5c540d1dd13d0b2d147def1c6c33abfc84a74
DJ3125 <dylanalexja@gmail.com>
2025-06-15 10:59:21 -0500

    First stable

--- src/Geometry.ts
+++ src/Geometry.ts
@@ -32,9 +32,16 @@ export class Triangle {
   }
   
   public isInside(vector: Point): boolean{
-    const newVector: Point = vector.subtractVector(this.points[0]);
-    const vector1: Point = this.points[1].subtractVector(this.points[0]);
-    const vector2: Point = this.points[2].subtractVector(this.points[0]);
-    return false;
+    const newVec: Point = vector.subtractVector(this.points[0]);
+    const vec1: Point = this.points[1].subtractVector(this.points[0]);
+    const vec2: Point = this.points[2].subtractVector(this.points[0]);
+    const det = vec1.getX() * vec2.getY() - vec2.getX() * vec1.getY();
+    if(det === 0){return false;}
+    const detInverse = det**-1;
+    const a = detInverse * (vec2.getY() * newVec.getX() - vec2.getX() * newVec.getY());
+    if(a <= 0 || a >= 1){return false;}
+    const b = detInverse * (vec1.getX() * newVec.getY() - vec1.getY() * newVec.getX());
+    if(b <= 0 || b >= (1 - a)){return false;}
+    return true;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,41 @@ import {Triangle, Point} from "./Geometry.js";
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
-let currentTriangle: Triangle = new Triangle(new Point(0, 0), new Point(0, 100), new Point(100, 0));
+let currentTriangle: Triangle;
+let x = -100, y = -100;
 
 window.onload = initialize;
 
 function initialize():void{  
   canvas = window.document.getElementById("canvas") as HTMLCanvasElement;
   ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-  
+  const button = document.getElementById("creator") as Element;
+  createNewTriangle();
+  canvas.addEventListener("mousemove", ({clientX, clientY} : MouseEvent)=>{
+    const rect = canvas.getBoundingClientRect();
+    x = (clientX - rect.x)/rect.width * canvas.width;
+    y = (clientY - rect.y)/rect.height * canvas.height;
+    //console.log("x=", x, ", y=", y);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawTriangle();
+  });
+  button.addEventListener("click", createNewTriangle);
+}
+
+function drawTriangle(){
   const points: Point[] = currentTriangle.getPoints();
+  ctx.save();
+  ctx.fillStyle = currentTriangle.isInside(new Point(x, y)) ? "blue" : "black";
   ctx.beginPath();
   ctx.moveTo(points[0].getX(), points[0].getY());
   ctx.lineTo(points[1].getX(), points[1].getY());
   ctx.lineTo(points[2].getX(), points[2].getY());
   ctx.fill();
+  ctx.restore();
+}
+
+function createNewTriangle(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  currentTriangle = new Triangle(new Point(Math.random() * canvas.width, Math.random() * canvas.height), new Point(Math.random() * canvas.width, Math.random() * canvas.height), new Point(Math.random() * canvas.width, Math.random() * canvas.height));
+  drawTriangle();
 }
